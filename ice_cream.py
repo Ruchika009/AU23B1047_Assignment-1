0fe1bf4575cb5f96dcd39fd2b8015b52d9fc8d6a
Ruchika Sonkusare <165674600+ruvhika@users.noreply.github.com>
2024-04-17 11:56:01 +0000

    P4 commit

--- ice_cream.py
+++ ice_cream.py
@@ -1,1 +1,16 @@
+def add_topping(Scoop_size,*toppings):
+    #Scoop_size=["small","medium","big"]
+    #toppings=["springles","hot fudge","whipped cream","crushed nuts"]
+    for j in toppings:
+        output=j
+    return toppings
+
+def make_shake(shake_favour):
+    if shake_favour=="nuts":
+        return shake_favour
+    elif shake_favour=="fruits":
+        return shake_favour
+    else:
+        return "no such shake available"
+    
 
--- making_icecream.py
+++ making_icecream.py
@@ -1,1 +1,9 @@
 
+from ice_cream import *
+
+toppings=add_topping("Small","springles","hot fudge","whipped cream")
+print("Toppings on icecream are: ")
+print(*toppings, sep='\n')
+
+shake=make_shake("nuts")
+print("Shake flavour choosen is ",shake)
