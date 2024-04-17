acde7720715b157a2d97264abccc54bf3577c0e5
Ruchika Sonkusare <165674600+ruvhika@users.noreply.github.com>
2024-04-17 12:03:31 +0000

    P6 commit

--- ROI_module.py
+++ ROI_module.py
@@ -1,1 +1,30 @@
 
+AS=int(input("Annual Site Profit: "))
+CCR=int(input("Current Conversion Rate: "))
+ICR=int(input("Improved Conversion Rate: "))
+IMC=int(input("Improvemenet Cost: "))
+EPL=int(input("Expected Project Life: "))
+i=0.05
+
+def Future_gain_from_improvement():
+    n=(AS*(ICR/CCR)-AS)*((1+i)**EPL-1)/i-IMC*(1+i)**EPL
+    return n
+
+print(Future_gain_from_improvement())
+
+def Total_gain_from_improvement():
+    l=Future_gain_from_improvement()/(i+1)**EPL
+    return l
+
+def Annnual_gain_from_improvement():
+    s=Total_gain_from_improvement()/EPL
+    return s
+
+def Annual_ROI():
+    a=Annnual_gain_from_improvement()/IMC
+    return a
+
+def Total_ROI():
+    b=Total_gain_from_improvement()/IMC
+    return b
+
--- ROI_usage.py
+++ ROI_usage.py
@@ -1,1 +1,7 @@
 
+from ROI_module import*
+
+print(Total_gain_from_improvement())
+print(Annnual_gain_from_improvement())
+print(Annual_ROI())
+print(Total_ROI())
