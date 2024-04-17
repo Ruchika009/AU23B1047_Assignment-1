50c0e1f8beafda4a8c1d556ef98334d24bba7ba4
Ruchika Sonkusare <165674600+ruvhika@users.noreply.github.com>
2024-04-17 11:36:23 +0000

    P1 commit

--- Problem1.py
+++ Problem1.py
@@ -1,1 +1,22 @@
 #get_tshirt function
+def get_tshirt(brand_name):
+    #get.name()
+    intialize='"Hii '
+    sen=', the brand you are looking for is available in store."'
+    sen1=', Unfortunately the brand you are looking for is not available in store."'
+    n=get_name()
+    available_brand=["Peter England", "Raymond","Arrow","Allen Solly","Gucci"]
+    for i in available_brand:
+        if brand_name==i:
+            greeting=intialize+n+sen
+            return greeting
+    else:
+        greeting=intialize+n+sen1
+        return greeting
+        
+
+def get_name():
+    name=input("Enter name:")
+    return name
+n=input("Enter brand name:")
+print(get_tshirt(n))
